a695433cf70c551190fc44baf637b648f244211f
DinithKumudika <dinithwalpitagama@gmail.com>
2023-06-15 11:21:27 +0530

    ocr answers

--- src/models/answer.py
+++ src/models/answer.py
@@ -1,4 +1,66 @@
 from pydantic import BaseModel
 
+import cv2
+import numpy as np
+from google.cloud import vision
+
+import os
+
+import config.config
+import helpers
+
 class Answer(BaseModel):
      pass
+
+def extract_answers(paper_no):
+     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.settings.GOOGLE_APPLICATION_CREDENTIALS
+     count = 0
+     
+     try:
+          images = helpers.get_images(os.path.join('./../data/images/', paper_no))
+          save_path = os.path.join('./../data/answers/', paper_no)
+          os.mkdir(save_path)
+     
+          for img_idx, image in enumerate(images):
+               src_image = cv2.imread(image)
+               screen_width = helpers.get_screen_width()
+               screen_height = helpers.get_screen_height() 
+               sized_image = helpers.resize(src_image, screen_height, screen_width)
+          
+               contours = helpers.detect_edges(sized_image)
+               answers = []
+          
+               for i, contour in enumerate(contours):
+                    # precision for approximation
+                    epsilon = 0.01 * cv2.arcLength(contour, True)
+                    approx = cv2.approxPolyDP(contour, epsilon, True)
+                         
+                    x, y, w, h = cv2.boundingRect(approx)
+                    aspect_ratio = w / h
+               
+                    if aspect_ratio > 1 and w > 50 and h > 20 and cv2.contourArea(contour) > 100:
+                         if w > 700:
+                              cropped_answer = sized_image[y:y+h, x:x+w]
+                              answers.append(cropped_answer)
+                              
+          for ans_idx, answer in enumerate(answers):
+               helpers.save_image(answer, save_path, ans_idx, img_idx)
+               count = count + 1
+          return str(count)
+     except OSError:
+          return None
+
+def read_answers(paper_no):
+     answers = []
+     client = vision.ImageAnnotatorClient()
+     answer_path = os.path.join('./../data/answers/', paper_no)
+     answer_images = helpers.get_images(answer_path)
+     
+     for i, image in enumerate(answer_images):
+          scanned_text = helpers.read_text(client, image)
+          answers.append({
+               "question no": i+1, 
+               "answer": scanned_text
+          })
+          
+     return answers
--- src/routes/answer.py
+++ src/routes/answer.py
@@ -2,75 +2,42 @@ from fastapi import APIRouter, HTTPException, status
 from fastapi.params import Body
 from bson.objectid import ObjectId
 
-import cv2
-import numpy as np
-from google.cloud import vision
-
-import os
-
-from config import config
 from config.database import Database
-from models.answer import Answer
+from models.answer import Answer, extract_answers, read_answers
 from schemas.answer import answerEntity, answersEntity
-import helpers
 
 database = Database()
 db = database.connect()
 users_collection = db["answer"]
 router = APIRouter(prefix="/answers")
 
-@router.get("/text/{paper_no}", response_description="extract answers as text")
+@router.post("/text/{paper_no}", response_description="extract answers as text")
 async def answer_to_text(paper_no):
-     answer_texts = []
-     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = config.settings.GOOGLE_APPLICATION_CREDENTIALS
      try:
-          images = helpers.get_images(os.path.join('./../data/images/', paper_no))
-          save_path = os.path.join('./../data/answers/', paper_no)
-          os.mkdir(save_path)
-          
-          client = vision.ImageAnnotatorClient()
-     
-          for img_idx, image in enumerate(images):
-               src_image = cv2.imread(image)
-               screen_width = helpers.get_screen_width()
-               screen_height = helpers.get_screen_height() 
-               sized_image = helpers.resize(src_image, screen_height, screen_width)
-          
-               contours = helpers.detect_edges(sized_image)
-               answers = []
-          
-               for i, contour in enumerate(contours):
-                    # precision for approximation
-                    epsilon = 0.01 * cv2.arcLength(contour, True)
-                    approx = cv2.approxPolyDP(contour, epsilon, True)
-                         
-                    x, y, w, h = cv2.boundingRect(approx)
-                    aspect_ratio = w / h
-               
-                    if aspect_ratio > 1 and w > 50 and h > 20 and cv2.contourArea(contour) > 100:
-                         if w > 700:
-                              cropped_answer = sized_image[y:y+h, x:x+w]
-                              answers.append(cropped_answer)
-          
-          for ans_idx, answer in enumerate(answers):
-               helpers.save_image(answer, save_path, ans_idx, img_idx)
-          
-          answer_images = helpers.get_images(save_path)
-          for i, image in enumerate(answer_images):
-               scanned_text = helpers.read_text(client, image)
-               answer_texts.append({
-                    "question no": i+1, 
-                    "text": scanned_text
-               })
-          return {"status": status.HTTP_200_OK, "answers": answer_texts}
+          no_of_answers = extract_answers(paper_no)
+          return {
+               "status": status.HTTP_200_OK, 
+               "message": f"{no_of_answers} answers saved"
+          }
      except OSError:
-          return {"error": "os error"}
+          return {
+               "status": status.HTTP_500_INTERNAL_SERVER_ERROR, 
+               "message": "couldn't extract answers"
+          }
 
           
-@router.post('/{paper_no}', response_description="save extracted answers to the database")
+@router.get('/text/{paper_no}', response_description="save extracted answers to the database")
 async def save_answers(paper_no):
-     client = vision.ImageAnnotatorClient()
-     answer_path = os.path.join('./../data/answers/', paper_no)
-     answer_images = helpers.get_images(answer_path)
-     for i, image in enumerate(answer_images):
-          scanned_text = helpers.read_text(client, image)
+     answers = read_answers(paper_no)
+     if answers:
+          return {"status": status.HTTP_200_OK, "answers": answers}
+     else:
+          return {
+               "status": status.HTTP_500_INTERNAL_SERVER_ERROR, 
+               "message": "error getting answers"
+          }
+
+          
+@router.post('/compare', response_description="compare between question text and marking scheme then returns similarity")
+async def check_similarity():
+     pass
