e04345dd6e86471a879bd23295e0791452228297
DinithKumudika <dinithwalpitagama@gmail.com>
2023-06-19 00:23:04 +0530

    auth router

--- src/api.py
+++ src/api.py
@@ -1,12 +1,15 @@
 # routes
 from fastapi import APIRouter
 
+from routes.auth import router as auth_router
 from routes.user import router as user_router
 from routes.paper import router as paper_router
 from routes.answer import router as answer_router
 
 router = APIRouter()
+
 # register routes
+router.include_router(auth_router, tags=["authentication"], prefix="/auth")
 router.include_router(user_router, tags=["users"], prefix="/users")
 router.include_router(paper_router,  tags=["papers"], prefix="/papers")
 router.include_router(answer_router, tags=["answers"], prefix="/answers")
