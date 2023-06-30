ef67bed1cec50de5865aba1aa78fc852ead831e1
rushinsandeepana <rushinsandeepana77@gmail.com>
2023-06-30 12:25:24 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
+import TopBar from "./Components/TopBar";
+import QuestionsPage from "./Pages/QuestionsPage";
 import EssayPage from './Pages/EssayPage';
-import Login from './Pages/Login';
-import Register from './Pages/Register';
-import AdminDashboard from './Pages/AdminDashboard';
-import SubjectsPage from './Pages/SubjectsPage';
-import SubjectPage from './Pages/SubjectPage';
-import YearsPage from './Pages/YearsPage';
-import MarkingSchemesPage from './Pages/MarkingSchemesPage';
-import AnswerSheetsPage from './Pages/AnswerSheetsPage';
-import TopBar from './Components/TopBar';
+import McqPage from './Pages/McqPage';
 import './Components/TopBar.css';
+import TeacherPage from './Pages/TeacherPage';
 
 function App(){
   return(
-    <div className='h-full'>
+    <div>
       {/* <TopBar /> */}
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        <Route path="/essayPage" element={<EssayPage />} />
-
-        <Route path="/subjects" element={<SubjectsPage />} />
-        <Route path="/subjects/:subjectId" element={<SubjectPage />} />
-        <Route path="/subjects/years/:subjectCode" element={<YearsPage />} />
-        <Route path="/subjects/:year/:subjectId" element={<MarkingSchemesPage />} />
-
-
-        <Route path="/markingschemes" element={<SubjectsPage />} />
-        <Route path="/markingschemes/years/:subjectCode" element={<YearsPage />} />
-        <Route path="/markingschemes/:year/:subjectId" element={<MarkingSchemesPage />} />
-
-        <Route path="/answersheets" element={<SubjectsPage />} />
-        <Route path="/answersheets/years/:subjectCode" element={<YearsPage />} />
-        <Route path="/answersheets/:year/:subjectId" element={<AnswerSheetsPage />} />
-
-        
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/" element={<QuestionsPage />} />
+        <Route path="essayPage" element={<EssayPage />} />
+        <Route path="mcqPage" element={<McqPage />} />
+        <Route path="teacherPage" element={<TeacherPage />} />
       </Routes>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,14 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {
-      height: {
-        'custom-8%': '8%',
-        'custom-95%': '95%',
-        'custom-90%': '90%',
-        'custom-94%': '94%',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
