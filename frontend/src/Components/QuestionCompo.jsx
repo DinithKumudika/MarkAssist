2fe3bf9b25501cb79fb13e7b755578ff00ec309b
rushinsandeepana <rushinsandeepana77@gmail.com>
2023-11-01 09:08:11 +0530

    QuestionCompo update

--- frontend/src/Components/QuestionCompo.jsx
+++ frontend/src/Components/QuestionCompo.jsx
@@ -11,9 +11,7 @@ function QuestionCompo() {
               
                 <div class="mb-10 space-y-4">
                   fgedgegg
-                    {/* <h3 class="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a> */}
+                    
                 </div>
                 {/* <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg" class="w-2/3 ml-auto -mb-12" alt="illustration" loading="lazy" width="900" height="600" /> */}
               
@@ -28,21 +26,10 @@ function QuestionCompo() {
         </div>
     </div>
     
-{/* <div class="flex rounded-md shadow-sm gap-10 mt-5" role="group">
-  <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-    Profile
-  </button>
-  <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-    Settings
-  </button>
-  <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-    Messages
-  </button>
-</div> */}
 
 </div>
     </div>
   )
 }
 
-export default QuestionCompo
+export default QuestionCompo;
